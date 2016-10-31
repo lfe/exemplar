5f3b656a1df59968144800e6187bfd4b353286e8
Duncan McGreggor <oubiwann@gmail.com>
2016-10-31 18:21:41 -0500

    Added support for testing all builds.

--- priv/make/code.mk
+++ priv/make/code.mk
@@ -2,18 +2,44 @@ compile:
 	rebar3 compile
 
 check:
-	@rebar3 as test lfe test
+	@echo
+	@echo "=================================="
+	@echo "Running tests using Github Sources ..."
+	@echo "=================================="
+	@echo
+	rebar3 as test lfe test
+
+check-gitlab:
+	@echo
+	@echo "=================================="
+	@echo "Running tests using Gitlab Sources ..."
+	@echo "=================================="
+	@echo
+	rebar3 as gitlab lfe test
+
+check-hexpm: clean
+	@echo
+	@echo "==================================="
+	@echo "Running tests using Hex.pm Packages ..."
+	@echo "==================================="
+	@echo
+	@rebar3 as hexpm lfe clean
+	rebar3 as hexpm lfe test
+
+check-all: check check-gitlab check-hexpm
 
 repl:
-	@rebar3 as dev compile
-	@$(LFE) -pa `rebar3 as dev path -s " -pa "`
+	rebar3 as $(REBAR_PROFILE) compile
+	$(LFE) -pa `rebar3 as $(REBAR_PROFILE) path -s " -pa "`
 
 shell:
 	@rebar3 shell
 
 clean:
 	@rebar3 clean
-	@rm -rf ebin/* _build/default/lib/$(PROJECT) rebar.lock
+	@rm -rf ebin/* _build/default/lib/$(PROJECT) rebar.lock \
+		rebar3.crashdump \
+		$(HOME)/.cache/rebar3/hex/default/packages/*
 
 clean-all: clean
 	@rebar3 as dev lfe clean
@@ -29,13 +55,31 @@ push-tags:
 push-all: push push-tags
 
 build-github: clean
+	@echo
+	@echo "============================="
+	@echo "Building using Github Sources ..."
+	@echo "============================="
+	@echo
 	rebar3 compile
+	rebar3 lock
 
 build-gitlab: clean
+	@echo
+	@echo "============================="
+	@echo "Building using Gitlab Sources ..."
+	@echo "============================="
+	@echo
 	rebar3 as gitlab compile
+	rebar3 as gitlab lock
 
 build-hexpm: clean
+	@echo
+	@echo "=============================="
+	@echo "Building using Hex.pm Packages ..."
+	@echo "=============================="
+	@echo
 	rebar3 as hexpm compile
+	rebar3 as hexpm lock
 
 build-all: build-github build-gitlab build-hexpm
 
